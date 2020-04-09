5d756b576ebe7b97e62817248a08436a44598e69
Cala 88 <mcalafell@cifpfbmoll.eu>
2020-04-09 17:09:50 +0200

    Index terminado

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# Llenguatges de Marques i Sistemes de Gestió d'Informació
+
 # CalafellEstarellasMiquel_Tasca9.3
+
+# Miquel Calafell Estarellas
--- index.js
+++ index.js
@@ -1,70 +1,74 @@
 var menu = require('node-menu');
 
 var alumnes_json =
-{
-  "alumnes":
   [
       {
           "dni": "36867720A",
           "nombre": "Paco",
           "apellido": "Gonzalez",
           "edad": 65,
-          "lista de asignaturas": [{"Mates": 8}, {"Biologia": 6}]
+          "lista_de_asignaturas": [{"Mates": 8}, {"Biologia": 6}]
       },
       {
           "dni": "45397689E",
           "nombre": "Benito",
           "apellido": "Martinez",
           "edad": 14,
-          "lista de asignaturas": [{"Mates": 7}, {"Biologia": 7}, {"Castellano": 5}, {"Ingles": 8}, {"E.F.": 9}]
+          "lista_de_asignaturas": [{"Mates": 7}, {"Biologia": 7}, {"Castellano": 5}, {"Ingles": 8}, {"E.F.": 9}]
       },
       {
           "dni": "45695378F",
           "nombre": "Donald",
           "apellido": "Trump",
           "edad": 73,
-          "lista de asignaturas": [{"Mates": 7}, {"Economia": 10}, {"Castellano": 5}]
+          "lista_de_asignaturas": [{"Mates": 7}, {"Economia": 10}, {"Castellano": 5}]
       },
       {
           "dni": "51228248H",
           "nombre": "Helen",
           "apellido": "Chufe",
           "edad": 33,
-          "lista de asignaturas": [{"Ingles": 5}, {"Biologia": 3}, {"Catalan": 4}]
+          "lista_de_asignaturas": [{"Ingles": 5}, {"Biologia": 3}, {"Catalan": 4}]
       },
       {
           "dni": "3971830M",
           "nombre": "Alex",
           "apellido": "Tintor",
           "edad": 40,
-          "lista_de_asignaturas": [{"Catalan": 8}, {"E.F.": 6}, {"Fisica": 9}]
+          "lista_de_asignaturas": [{'Catalan': 8}, {'E.F.': 6}, {'Fisica': 9}]
       }
   ]
-}
  
 menu.addDelimiter('-', 40, 'Main Menu')
-    .addItem(
+menu.addItem(
         'Mostrar la llista d’alumnes',
-        function num1() {console.log(alumnes_json);})
+        function listarAlumnes() {console.log(alumnes_json);});
     
-    .addItem(
+menu.addItem(
         'Afegir un alumne',
-        function num2(valor1, valor2, valor3, valor4, valor5) {
-          alumnes_json.alumnes.dni = valor1;
-          alumnes_json.alumnes.nombre = valor2;
-          alumnes_json.alumnes.apellido = valor3;
-          alumnes_json.alumnes.edad = valor4;
-          alumnes_json.alumnes.lista_de_asignaturas = valor5;
-           
+        function añadirAlumne(valor1, valor2, valor3, valor4, valor5) {
+          alumnes_json.dni = valor1;
+          alumnes_json.nombre = valor2;
+          alumnes_json.apellido = valor3;
+          alumnes_json.edad = valor4;
+          alumnes_json.lista_de_asignaturas = valor5;
+        
           var myString = JSON.stringify(alumnes_json);
-            })
+          console.log("Nuevo alumno añadido:", alumnes_json);
+            },
+            null, 
+            [{'name': 'dni', 'type': 'string'}, {'name': 'nombre', 'type': 'string'}, {'name': 'apellido', 'type': 'string'},
+            {'name': 'edad', 'type': 'numeric'}, {'name': 'lista_de_asignaturas', 'type': 'string'}]);
 
-    .addItem(
+menu.addItem(
             'Esborrar un alumne',
-            function eliminarPorDni(dni){
-              delete alumnes_json[dni];
-                console.log(alumnes_json);
-            })
+            function eliminarPerDni(dni) {
+                const index = alumnes_json.findIndex(x => x.dni === dni);
+                if (index !== undefined) alumnes_json.splice(index, 1);
+                console.log("Alumno eliminado:", alumnes_json);
+            },
+            null, 
+            [{'name': 'dni', 'type': 'string'}]);
           
-    .addDelimiter('*', 40)
-    .start();
+menu.addDelimiter('*', 40)
+menu.start();
